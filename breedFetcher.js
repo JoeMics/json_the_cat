a2ba30840c46be30e81e1058c2dd77630d89e953
Joseph Micla <josephmicla@gmail.com>
2021-11-04 19:43:33 +0000

    makes requests to cat api, and returns the description of the cat breed if found

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,16 @@
 const request = require('request');
+
+const catBreed = process.argv[2];
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`, (err, res, body) => {
+  // handle errors
+  if (err) throw `Oops, something went wrong: ${err}`;
+
+  if (body === '[]') {
+    // Empty array returned by API if nothing matches the query
+    console.log('Sorry, no breed found!');
+    return;
+  }
+  const data = JSON.parse(body);
+  console.log(data[0].description);
+});
